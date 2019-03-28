f9b1cc3ee3d286f575bd89bc05bedbc0d50da5d5
ceol22 <jordywk2004@gmail.com>
2019-03-28 17:39:07 +0100

    Fixed layout

--- src/RFID Lockers.ino
+++ src/RFID Lockers.ino
@@ -44,95 +44,97 @@ void setup()
   lcd.setCursor(0,1);
   lcd.print("Scan uw pas");
 }
-void loop()
-{
+
+
+void loop(){
+
   while(x==0){
 
-  int buttonVal = digitalRead(button1);
-  // Look for new cards
-  if ( ! mfrc522.PICC_IsNewCardPresent())
-  {
-    return;
-  }
-  // Select one of the cards
-  if ( ! mfrc522.PICC_ReadCardSerial())
-  {
-    return;
-  }
-  //Show UID on serial monitor
-  Serial.print("UID tag :");
-  String content= "";
-  byte letter;
-  for (byte i = 0; i < mfrc522.uid.size; i++)
-  {
-     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-     Serial.print(mfrc522.uid.uidByte[i], HEX);
-     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     content.concat(String(mfrc522.uid.uidByte[i], HEX));
-  }
-  Serial.println();
-  Serial.print("Message : ");
-  content.toUpperCase();
-
-  if (content.substring(1) == "61 57 CC 09") //Leeg
-  {
-    Serial.println("Authorized access");
-    kluis1.write(90);
-    kluis2.write(90);
-    kluis3.write(90);
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Waarschuwing!");
-    lcd.setCursor(0,1);
-    lcd.print("Alles geopend");
-    x = 1;
-    delay(3000);
-  }
-
-  if (content.substring(1) == "C3 30 90 E5") //Mickey
-  {
-    Serial.println("Authorized access");
+    int buttonVal = digitalRead(button1);
+    // Look for new cards
+    if ( ! mfrc522.PICC_IsNewCardPresent())
+    {
+      return;
+    }
+    // Select one of the cards
+    if ( ! mfrc522.PICC_ReadCardSerial())
+    {
+      return;
+    }
+    //Show UID on serial monitor
+    Serial.print("UID tag :");
+    String content= "";
+    byte letter;
+    for (byte i = 0; i < mfrc522.uid.size; i++)
+    {
+      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+      Serial.print(mfrc522.uid.uidByte[i], HEX);
+      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+      content.concat(String(mfrc522.uid.uidByte[i], HEX));
+    }
     Serial.println();
-    kluis1.write(90);
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Hoi Mickey Doos");
-    lcd.setCursor(0,1);
-    lcd.print("Snellerder");
-    x = 1;
-    delay(3000);
-  }
+    Serial.print("Message : ");
+    content.toUpperCase();
 
-    if (content.substring(1) == "82 9C 1B 4E") //Bryan
-  {
-    Serial.println("Authorized access");
-    Serial.println();
-    kluis2.write(90);
-      lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Mui Olde rukker");
-    lcd.setCursor(0,1);
-    lcd.print("Anmak'n");
-    x = 1;
-    delay(3000);
-  }
+    if (content.substring(1) == "61 57 CC 09") //Leeg
+    {
+        Serial.println("Authorized access");
+        kluis1.write(90);
+        kluis2.write(90);
+        kluis3.write(90);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Waarschuwing!");
+        lcd.setCursor(0,1);
+        lcd.print("Alles geopend");
+        x = 1;
+        delay(3000);
+    }
+
+    if (content.substring(1) == "C3 30 90 E5") //Mickey
+    {
+        Serial.println("Authorized access");
+        Serial.println();
+        kluis1.write(90);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Hoi Mickey Doos");
+        lcd.setCursor(0,1);
+        lcd.print("Snellerder");
+        x = 1;
+        delay(3000);
+    }
+
+      if (content.substring(1) == "82 9C 1B 4E") //Bryan
+      {
+        Serial.println("Authorized access");
+        Serial.println();
+        kluis2.write(90);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Mui Olde rukker");
+        lcd.setCursor(0,1);
+        lcd.print("Anmak'n");
+        x = 1;
+        delay(3000);
+      }
 
 
-  if (content.substring(1) == "9B EE F9 24") //Jelle
-  {
-    Serial.println("Authorized access");
-    Serial.println();
-    kluis3.write(90);
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("Hoi Jelle");
-    lcd.setCursor(0,1);
-    lcd.print("Doe maar rustig");
-    x = 1;
-    delay(3000);
-  }
+      if (content.substring(1) == "9B EE F9 24") //Jelle
+      {
+        Serial.println("Authorized access");
+        Serial.println();
+        kluis3.write(90);
+        lcd.clear();
+        lcd.setCursor(0,0);
+        lcd.print("Hoi Jelle");
+        lcd.setCursor(0,1);
+        lcd.print("Doe maar rustig");
+        x = 1;
+        delay(3000);
+      }
 
-
+      
 
 
   }
@@ -152,10 +154,10 @@ void loop()
     lcd.print("Kalm an doen");
     delay(2000);
     lcd.clear();
-     lcd.setCursor(0,0);
-  lcd.print("Welkom,");
-  lcd.setCursor(0,1);
-  lcd.print("Scan uw pas");
+    lcd.setCursor(0,0);
+    lcd.print("Welkom,");
+    lcd.setCursor(0,1);
+    lcd.print("Scan uw pas");
     x = 0;
 
   }
